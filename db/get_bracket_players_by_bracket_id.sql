c692a19b8ee5f6673932a03ca7b1c92e87fb220f
Larry Li <larry.zy.li@gmail.com>
2018-01-22 00:58:36 -0600

    finished removing seeding

--- db/get_bracket_players_by_bracket_id.sql
+++ db/get_bracket_players_by_bracket_id.sql
@@ -1,2 +1,2 @@
-SELECT bracket_id, u.user_id AS id, seed, joined_at, alias AS name FROM bracket_player bp 
+SELECT bracket_id, u.user_id AS id, joined_at, alias AS name FROM bracket_player bp 
 JOIN "user" AS u ON bp.user_id=u.user_id WHERE bp.bracket_id = $1 ORDER BY joined_at ASC;
